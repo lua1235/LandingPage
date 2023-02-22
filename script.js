7698b307e2e0acde07b296ba35085121d8e5769c
lua1235 <124845051+lua1235@users.noreply.github.com>
2023-02-22 10:57:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 let name = prompt("What is your name?");
 let goal = prompt("What is your goal for today?");
 document.getElementById("nameSpan").innerHTML = name;
-document.getElementById("Focus").innerHTML = goal;
+document.getElementById("focuSpan").innerHTML = goal;
