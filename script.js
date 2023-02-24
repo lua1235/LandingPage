0f49ca1bd5c82bb89b86bf1a1799c5f6d2879ad8
lua1235 <124845051+lua1235@users.noreply.github.com>
2023-02-24 09:20:45 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+let name = prompt("What is your name?");
+let goal = prompt("What is your goal for today?");
+document.getElementById("nameSpan").innerHTML = name;
+document.getElementById("focuSpan").innerHTML = goal;
