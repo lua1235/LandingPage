183a6a2a7ee37563fdebe4e1147979850ce918aa
lua1235 <124845051+lua1235@users.noreply.github.com>
2023-02-24 09:15:32 -0700

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-let name = prompt("What is your name?");
-let goal = prompt("What is your goal for today?");
-document.getElementById("nameSpan").innerHTML = name;
-document.getElementById("focuSpan").innerHTML = goal;
