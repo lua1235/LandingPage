5c9219d371f6b474938da341ab6e305471302d8c
lua1235 <124845051+lua1235@users.noreply.github.com>
2023-04-05 10:11:19 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let name = prompt("What is your name?");
 let goal = prompt("What is your goal for today?");
+let quote = prompt("What is your inspirational quote for today?")
+let source = prompt("Who was responsible for your quote?")
 document.getElementById("nameSpan").innerHTML = name;
 document.getElementById("focuSpan").innerHTML = goal;
+document.getElementById("quoteSpan").innerHTML = quote;
+document.getElementById("speakerSpan").innerHTML = source;
